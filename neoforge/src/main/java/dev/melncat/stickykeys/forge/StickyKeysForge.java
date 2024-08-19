fd4bd2408b650978bb093dd055a9e6db64e27116
MelnCat <melncatuwu@gmail.com>
2024-08-18 22:32:29 -0700

    porting

--- neoforge/src/main/java/dev/melncat/stickykeys/forge/StickyKeysForge.java
+++ neoforge/src/main/java/dev/melncat/stickykeys/forge/StickyKeysForge.java
@@ -1,35 +1,23 @@
 package dev.melncat.stickykeys.forge;
 
-import dev.architectury.event.events.client.ClientTickEvent;
 import dev.architectury.platform.Platform;
-import dev.architectury.platform.forge.EventBuses;
 import dev.architectury.utils.Env;
-import dev.isxander.yacl3.api.YetAnotherConfigLib;
-import dev.melncat.stickykeys.config.StickyKeysConfig;
-import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.ConfigScreenHandler;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
 import dev.melncat.stickykeys.StickyKeys;
+import dev.melncat.stickykeys.config.StickyKeysConfig;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(StickyKeys.MOD_ID)
 public final class StickyKeysForge {
 	public StickyKeysForge() {
 		if (Platform.getEnvironment() == Env.SERVER) return;
 
-		EventBuses.registerModEventBus(StickyKeys.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-
 		StickyKeys.init();
 
 		ModLoadingContext.get().registerExtensionPoint(
-			ConfigScreenHandler.ConfigScreenFactory.class,
-			() -> new ConfigScreenHandler.ConfigScreenFactory(
-				(client, parent) -> StickyKeysConfig.createScreen(parent)
-			)
+			IConfigScreenFactory.class,
+			() -> (client, parent) -> StickyKeysConfig.createScreen(parent)
 		);
 	}
 }
